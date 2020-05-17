a5cbf212498bb366b91f2c7163d9538726b94d4b
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-05-17 15:27:49 +0900

    revised item width for responsive

--- src/components/InputInfo/InputInfoContent.js
+++ src/components/InputInfo/InputInfoContent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./InputInfoContent.css"
-import {  TextField } from "@material-ui/core"
+import { TextField } from "@material-ui/core"
 import CreateButton from "./CreateButton";
 export default class InputInfoContent extends React.Component {
     constructor(props) {
@@ -18,10 +18,10 @@ export default class InputInfoContent extends React.Component {
     }
     handleClick() {
         const testInfoIsCorrect = (this.state.testname.length <= 10) && (this.state.description.length > 0);
-        if(testInfoIsCorrect){
-            const {testname,description} = this.state;
-            this.props.addTest(testname,description);
-        }else{
+        if (testInfoIsCorrect) {
+            const { testname, description } = this.state;
+            this.props.addTest(testname, description);
+        } else {
             alert("入力が正しくありません");
         }
     }
@@ -29,31 +29,29 @@ export default class InputInfoContent extends React.Component {
     render() {
         return (
             <div className="inputInfoContent">
-                <div className="testname">
-                    {/* <InputLabel htmlFor="testname" ><p><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5 4v3h5.5v12h3V7H19V4z" /><path d="M0 0h24v24H0V0z" fill="none" /></svg>タイトル(10字以内)</p></InputLabel> */}
-                    <TextField
-                        id="standard-textarea"
-                        label="新しいテスト名"
-                        placeholder="ここに入力してください"
-                        variant="outlined"
-                        name="testname"
-                        onChange={this.handleChange}
-                        fullWidth
-                    />
-                </div>
-                <div className="description">
-                    <TextField
-                        id="standard-textarea"
-                        label="説明"
-                        placeholder="ここに入力してください"
-                        multiline
-                        rows={4}
-                        variant="outlined"
-                        name="description"
-                        onChange={this.handleChange}
-                        fullWidth
-                    />
-                </div>
+                {/* <InputLabel htmlFor="testname" ><p><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M5 4v3h5.5v12h3V7H19V4z" /><path d="M0 0h24v24H0V0z" fill="none" /></svg>タイトル(10字以内)</p></InputLabel> */}
+                <TextField
+                    id="standard-textarea"
+                    label="新しいテスト名"
+                    placeholder="ここに入力してください"
+                    variant="outlined"
+                    name="testname"
+                    onChange={this.handleChange}
+                    fullWidth
+                />
+                <br />
+                <br />
+                <TextField
+                    id="standard-textarea"
+                    label="説明"
+                    placeholder="ここに入力してください"
+                    multiline
+                    rows={4}
+                    variant="outlined"
+                    name="description"
+                    onChange={this.handleChange}
+                    fullWidth
+                />
                 <CreateButton handleClick={this.handleClick} />
             </div>
         )
--- src/components/Top/DescItems.js
+++ src/components/Top/DescItems.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DescItem from "./DescItem";
-import correctRateUrl from "./static/correctRate.png";
+import testBeforeUrl from "./static/testBefore.png";
 import createTestUrl from "./static/createTest.png";
 import resultShowUrl from "./static/resultShow.png"
 import "./DescItems.css";
@@ -8,9 +8,9 @@ import "./DescItems.css";
 export default function DescItems() {
     return (
         <div className="descItems">
-            <DescItem imgUrl={correctRateUrl} imgAlt="テスト作成" description="テストを作成します" />
+            <DescItem imgUrl={testBeforeUrl} imgAlt="テスト作成" description="テストを作成します" />
             <DescItem imgUrl={createTestUrl} imgAlt="" description="問題の正答率を保存し、苦手な問題をを重点的に学べます" />
             <DescItem imgUrl={resultShowUrl} imgAlt="" description="それまでの結果を示すグラフで、自ら成長を実感することも可能です" />
         </div>
-    )
+    );
 }
